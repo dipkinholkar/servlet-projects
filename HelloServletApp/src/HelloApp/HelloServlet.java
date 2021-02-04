279ef5662f40d4f661b319df04465a470f91a94d
Dipak Kinholkar <39441178+dipkinholkar@users.noreply.github.com>
2021-02-04 22:38:51 +0530

    HelloServletApp
    version 1.0

---
+++ HelloServletApp/src/HelloApp/HelloServlet.java
@@ -0,0 +1,65 @@
+package HelloApp;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import javax.servlet.ServletConfig;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * Servlet implementation class HelloServlet
+ */
+@WebServlet("/HelloServlet")
+public class HelloServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+
+    /**
+     * Default constructor. 
+     */
+    public HelloServlet() {
+        
+    System.out.println("constructor");
+    
+    }
+
+	/**
+	 * @see Servlet#init(ServletConfig)
+	 */
+	public void init(ServletConfig config) throws ServletException {
+	
+		System.out.println("servlet started...");
+	}
+
+	
+	public void destroy() {
+	
+	System.out.println("Servlet destroyed...");
+	}
+
+	/**
+	 * @see HttpServlet#service(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void service(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+	
+		try {
+		PrintWriter pw=response.getWriter();
+	
+		response.setContentType("text/html");
+	
+		String message=request.getParameter("msg");
+		
+		pw.print("Hello :"+message);
+	
+		}
+		catch(Exception e) {
+		
+			System.out.print(e);
+		}
+		}
+	
+	}
+
